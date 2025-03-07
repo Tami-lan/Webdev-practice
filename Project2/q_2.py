e39eb0d1d18226f1873a4f777911318d4601b4d0
Tami-lan T <upgradestamila@gmail.com>
2025-03-07 10:48:51 +0000

    new fie added

---
+++ Project2/q_2.py
@@ -0,0 +1,15 @@
+# Doing operaiton on input
+
+print("Enter the A Variable")
+a=int(input())
+print("Enter the B Variable")
+b=int(input())
+print("Enter the C Variable")
+c=int(input())
+
+mul=a*b*c
+add=a+b+c
+result=mul/add
+result2=mul//add
+print("The evaluation result",result)
+print("The evaluation resul2",result2)
---
+++ Project2/q_3.py
@@ -0,0 +1,14 @@
+#Mark evaluation
+
+print("Enter the Name")
+name=input()
+print("Enter the Score")
+score=int(input())
+print("Enter the department")
+dept=input()
+
+print("Name of the student:",name)
+print("Score of the student:",score/10,"/10")
+print("Department of the student:",dept)
+
+
