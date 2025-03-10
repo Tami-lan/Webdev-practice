67aa077f0f2d578a6f60d04443dca0dd3aa653b8
Tami-lan T <upgradestamila@gmail.com>
2025-03-10 06:38:39 +0000

    newfile is added

---
+++ Project2/q7_ifelse.py
@@ -0,0 +1,5 @@
+num=int(input("Enter the number to find is it divisible by 3 0r 5:"))
+if ((num%3)==(num%5)):
+    print("The given number is divisible by 3 and 5")
+else:
+    print("The given number is not divisible by 3 and 5")
